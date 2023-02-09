a4eda7d429b222fcc512ff73fd28dc74aa1b4f36
siria <siria55lee@gmail.com>
2023-02-09 11:24:43 +0800

    Camel2underscore (#7)
    * Add camelCase2underscore & test cases
    
    * add github actions
    
    * update version

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-const getCamelCase = (str) => {
+const formatTypes = {
+  UNDERSCORE: 'underscore',
+  CAMELCASE: 'camelCase'
+}
+
+const underscore2CamelCase = (str) => {
   if (!str.includes('_'))
     return str;
   const words = str.split('_');
@@ -7,19 +12,41 @@ const getCamelCase = (str) => {
   return firstWord + otherWords;
 }
 
-const getCamelCaseObject = (obj) => {
+const camelCase2underscore = (str) => {
+  let l = 0, r = 0;
+  const words = [];
+  while (r <= str.length) {
+    if (r == str.length || str[r] === str[r].toUpperCase()) {
+      let word = str.slice(l, r).toLowerCase();
+      words.push(word);
+      l = r;
+    }
+    ++r;
+  }
+  return words.join('_');
+}
+
+const formatObject = (obj, formatType) => {
+  let convertFunc;
+  if (formatType === formatTypes.CAMELCASE)
+    convertFunc = underscore2CamelCase;
+  else if (formatType === formatTypes.UNDERSCORE)
+    convertFunc = camelCase2underscore;
+
   for (const k in obj) {
-    if (!k.includes('_'))
-      continue;
-    const newK = getCamelCase(k);
+    const newK = convertFunc(k);
     Object.defineProperty(obj, newK, Object.getOwnPropertyDescriptor(obj, k));
-    delete obj[k];
+    if (k !== newK)
+      delete obj[k];
   }
   return obj;
 }
 
 const GFormat = {
-  getCamelCaseObject: getCamelCaseObject
+  underscore2CamelCase,
+  camelCase2underscore,
+  formatObject,
+  formatTypes,
 };
 
 module.exports = GFormat;
